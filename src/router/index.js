f16e708b72df34543c429289abb9e51b8df4c862
mianjvren <207921878@qq.com>
2020-09-07 11:42:49 +0800

    '初始化'

---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import { Home } from './assembly'
+export default class Router extends React.Component {
+    render() {
+        return (
+            <BrowserRouter>
+                <Switch>
+
+                    <Route path="/home" component={Home} />
+
+                </Switch>
+            </BrowserRouter>
+        );
+    }
+}
